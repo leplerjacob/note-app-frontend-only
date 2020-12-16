3cf9f9f0ab9f015fa4a5a865d01492b0d26a7d33
jl <jacoblepler91@gmail.com>
2020-12-16 07:17:09 -0500

    adding jest tests

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
-const Note = ({ note, toggledImportance, deleteNote }) => {
+const Note = ({ note, toggledImportance, deleteNote}) => {
   const label = note.important ? 'make not important' : 'make important'
 
   return (
-    <div>
       <li className="note">
         {note.content}
         <button onClick={toggledImportance}>{label}</button>
         <button onClick={deleteNote}>Delete</button>
       </li>
-    </div>
   )
 }
 
--- src/tests/Note.test.js
+++ src/tests/Note.test.js
@@ -1,17 +1,61 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
+import { prettyDOM } from '@testing-library/dom'
 import Note from '../components/Note'
 
-test('renders content', () => {
-  const note = {
-    content: 'Component testing is done with react-testing library',
-    important: true,
-  }
+describe('tests component properties', () => {
+  test('renders content', () => {
+    const note = {
+      content: 'Component testing is done with react-testing library',
+      important: true,
+    }
 
-  const component = render(<Note note={note} />)
+    const component = render(<Note note={note} />)
 
-  expect(component.container).toHaveTextContent(
-    'Component testing is done with react-testing-library'
-  )
+    //   component.debug()
+
+    const li = component.container.querySelector('li')
+
+    // console.log(prettyDOM(li))
+
+    //   method 1
+    expect(component.container).toHaveTextContent(
+      'Component testing is done with react-testing librarymake not importantDelete'
+    )
+
+    //   method 2
+    const element = component.getByText(
+      'Component testing is done with react-testing library'
+    )
+    expect(element).toBeDefined()
+
+    //   method 3
+    const div = component.container.querySelector('.note')
+    expect(div).toHaveTextContent(
+      'Component testing is done with react-testing library'
+    )
+  })
+})
+
+describe('tests component functionalities', () => {
+  test('clicking the button calls event handler once', async () => {
+    const note = {
+      content: 'Component testing is done with react-testing-library',
+      important: true,
+    }
+
+    const mockHandler = jest.fn()
+
+    const component = render(
+      <Note note={note} toggleImportance={mockHandler()} />
+    )
+
+    // component.debug()
+
+    const button = component.getByText('make not important')
+    fireEvent.click(button)
+
+    expect(mockHandler.mock.calls.length).toBe(1)
+  })
 })
