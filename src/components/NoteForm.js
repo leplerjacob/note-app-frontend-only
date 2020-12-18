65c11062a4faeefb16f955a36046750f716f3214
jl <jacoblepler91@gmail.com>
2020-12-17 22:54:33 -0500

    before adding cypress

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -20,7 +20,7 @@ const NoteForm = ({ createNote }) => {
   }
 
   return (
-    <div>
+    <div className='formDiv'>
       <h2>Create a new note</h2>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleChange} />
